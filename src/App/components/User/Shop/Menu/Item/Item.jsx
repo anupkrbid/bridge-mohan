c553a38e49a4d6547b909746c1272d550d657c40
anupkrbid <anupbid94@gmail.com>
2018-09-13 05:19:38 +0530

    Modified checkbox hiding logic for khilonewala

--- src/App/components/User/Shop/Menu/Item/Item.jsx
+++ src/App/components/User/Shop/Menu/Item/Item.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 import './Item.css';
 
-const item = props => (
-  <div className="row menu_row">
-    <div className="col-1 col-sm-1 col-md-1">
+const item = props => {
+  let checkbox = null;
+  if (props.shopName.toLowerCase() !== 'khilonewala') {
+    checkbox = (
       <input
         type="checkbox"
         style={{
@@ -21,15 +22,20 @@ const item = props => (
         }}
         aria-label={'Checkbox to to buy' + props.item.name}
       />
+    );
+  }
+  return (
+    <div className="row menu_row">
+      <div className="col-1 col-sm-1 col-md-1">{checkbox}</div>
+      <div className="col-7 col-sm-7 col-md-8">
+        <h3 className="menu_ttl_txt">{props.item.name}</h3>
+        {/* <p className="menu_dsc">This is demo text and will be replaced</p> */}
+      </div>
+      <div className="col-3 col-sm-3 col-md-3">
+        LMC <b>{props.item.price}</b>
+      </div>
     </div>
-    <div className="col-7 col-sm-7 col-md-8">
-      <h3 className="menu_ttl_txt">{props.item.name}</h3>
-      {/* <p className="menu_dsc">This is demo text and will be replaced</p> */}
-    </div>
-    <div className="col-3 col-sm-3 col-md-3">
-      LMC <b>{props.item.price}</b>
-    </div>
-  </div>
-);
+  );
+};
 
 export default item;
